96823790ee4c1175e79faa6d49885d43880f168e
Kelvin Gonçalves <kelvingoncalvestecinfo@gmail.com>
2021-01-04 13:54:09 -0300

    [ SPRINT - 1 ] Criação do projeto SPRING MAVEN

---
+++ src/test/java/br/com/entrevistaProvider/calculoimposto/CalculoImpostoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.entrevistaProvider.calculoimposto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculoImpostoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
